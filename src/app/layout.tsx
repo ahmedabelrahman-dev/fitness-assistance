779a6f8e15653573ccd061ee1765caa3db83ca44
ahmedabelrahman-dev <rman79340@gmail.com>
2026-01-04 21:15:13 +0400

    the app layout style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { type Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import ConvexClerkProvider from '../providers/ConvexClerkProvider';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,8 +31,16 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <header className="flex justify-end items-center p-4 gap-4 h-16"></header>
-          {children}
+          <Navbar />
+
+          {/* GRID BACKGROUND */}
+          <div className="fixed inset-0 -z-1">
+            <div className="absolute inset-0 bg-gradient-to-b from-background via-background to-background"></div>
+            <div className="absolute inset-0 bg-[linear-gradient(var(--cyber-grid-color)_1px,transparent_1px),linear-gradient(90deg,var(--cyber-grid-color)_1px,transparent_1px)] bg-[size:20px_20px]"></div>
+          </div>
+
+          <main className="pt-24 flex-grow">{children}</main>
+          <Footer />
         </body>
       </html>
     </ConvexClerkProvider>
